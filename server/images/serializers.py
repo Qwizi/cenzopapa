8674ca089ba148124b4fed6ce571cb6d8ddb6ea0
Adrian Ciołek <qwizi95@protonmail.com>
2021-11-11 15:09:06 +0100

    Added images list api

---
+++ server/images/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from images.models import Image
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = '__all__'
+
+
+class ImageOutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ['id', 'public_url', 'width', 'height', 'posted_at']
---
+++ server/images/views.py
@@ -0,0 +1,23 @@
+from random import randint
+
+from django.db.models import Max
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from images.models import Image
+from images.serializers import ImageOutSerializer
+
+
+class ImageViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Image.objects.all().order_by('-posted_at')
+    serializer_class = ImageOutSerializer
+
+    @action(detail=False)
+    def random(self, request):
+        max_id = Image.objects.all().aggregate(max_id=Max("id"))['max_id']
+        pk = randint(1, max_id)
+        random_image = Image.objects.get(pk=pk)
+        serializer = self.get_serializer(random_image)
+        return Response(serializer.data)
+
