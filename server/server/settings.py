87526cb8ec6054b5a553aa72352ad2a6833b8bd6
Adrian Ciołek <qwizi95@protonmail.com>
2021-11-15 19:27:28 +0100

    Changes in docker-compose

--- server/server/settings.py
+++ server/server/settings.py
@@ -19,10 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!h*^3rd3-$2z80evr4qqq7dcv!27rlu&-%-nw0=c9(&i*%mn!q'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = ['*']
 
