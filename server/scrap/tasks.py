7f584a1cbd620e945c8df63276fa6d7679ee66d0
Adrian Ciołek <qwizi95@protonmail.com>
2021-11-15 16:31:37 +0100

    Working scrap images from wykop

--- server/scrap/tasks.py
+++ server/scrap/tasks.py
@@ -1,19 +1,6 @@
-from images.models import Image
 from scrap.utils import ScrapService
 
 
-def scrap_images(days: int = 365, years: int = 2):
+def scrap_images(days: int = 7, years: int = None):
     scrap_service = ScrapService()
-    images = scrap_service.scrap(days=days, years=years)
-    for image in images:
-        if not Image.objects.filter(remote_image_url=image['remote_image_url']).exists():
-            new_image = Image.objects.create(
-                remote_image_url=image['remote_image_url'],
-                posted_at=image['posted_at'],
-                width=image['width'],
-                height=image['height'],
-                public_url=image['remote_image_url'],
-                author_id=1,
-            )
-            print(new_image)
-
+    scrap_service.scrap(days=days, years=years)
