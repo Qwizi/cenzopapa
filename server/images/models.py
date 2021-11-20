44a1ef272f50c7ebcb0d823d94e88f29d75b8fdb
Adrian Ciołek <=>
2021-11-20 19:28:48 +0100

    Updated image model field

--- server/images/models.py
+++ server/images/models.py
@@ -7,12 +7,12 @@ from django.dispatch import receiver
 
 
 class Image(models.Model):
-    url = models.URLField(default='http://localhost.pl/image.png')
+    url = models.URLField(null=True, blank=True)
     file = CloudinaryField('file', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     posted_at = models.DateTimeField()
-    height = models.IntegerField(default=600)
-    width = models.IntegerField(default=400)
+    height = models.IntegerField(null=True, blank=True)
+    width = models.IntegerField(null=True, blank=True)
     is_validated = models.BooleanField(default=False)
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -29,14 +29,27 @@ def on_create_image(sender, **kwargs):
         image = kwargs['instance']
         if image.file:
             image.url = image.file.url
+            image.height = image.file.metadata.get("height")
+            image.width = image.file.metadata.get("width")
             image.save()
 
 
 post_save.connect(on_create_image, sender=Image)
 
 
+@receiver(post_save, sender=Image)
+def save_image(sender, instance, **kwargs):
+    if instance.file:
+        if instance.file.url:
+            instance.url = instance.file.url
+
+        if instance.file.metadata:
+            instance.height = instance.file.metadata.get("height")
+            instance.width = instance.file.metadata.get("width")
+
+
 @receiver(post_delete, sender=Image)
-def save_profile(sender, instance, **kwargs):
+def delete_image_in_cloudinary(sender, instance, **kwargs):
     if instance.file:
         file_name = instance.file
         cloudinary.uploader.destroy(file_name.public_id, invalidate=True)
