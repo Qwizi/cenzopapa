a4e4424b9760290f9e51ee03747e3f856706c4d9
Adrian Ciołek <=>
2021-11-20 19:45:12 +0100

    Small changes

--- server/server/urls.py
+++ server/server/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('', include('djoser.urls')),
     url(r'^auth/', include('djoser.urls.jwt')),
+    path('', include('djoser.urls')),
     path('', include('images.urls'))
 ]
