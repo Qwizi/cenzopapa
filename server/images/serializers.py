5a1bd0522c587d957711a0f9eb69063a8245eb0e
Adrian Ciołek <=>
2021-11-21 15:13:55 +0100

    Added likes count to image serializer

--- server/images/serializers.py
+++ server/images/serializers.py
@@ -10,8 +10,12 @@ class ImageSerializer(serializers.ModelSerializer):
 
 
 class ImageOutSerializer(serializers.ModelSerializer):
+    likes_count = serializers.IntegerField(
+        source='likes.count',
+        read_only=True
+    )
     class Meta:
         model = Image
-        fields = ['id', 'url', 'width', 'height', 'posted_at', 'author']
+        fields = ['id', 'url', 'width', 'height', 'posted_at', 'author', 'likes', 'likes_count']
         read_only_fields = ['id', 'url', 'width', 'posted_at', 'author']
         write_fields = ['file']
