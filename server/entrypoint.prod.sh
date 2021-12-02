f89632e3cf916aadaf8d2d36a0c8bc83223d872c
Qwizi <ciolek.adrian@protonmail.com>
2021-12-02 20:59:47 +0100

    changed entrypoint

--- server/entrypoint.prod.sh
+++ server/entrypoint.prod.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
+# Wait for the postgres container to finish booting before running whatever
+# we're gonna run.
+#
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
+DB_NAME=cenzopapa
+DB_PORT=5432
 
-    echo "PostgreSQL started"
-fi
+printf "Waiting for psql to boot..."
 
-python manage.py migrate --noinput
+for _ in `seq 0 100`; do
+    (echo > /dev/tcp/${DB_NAME}/${DB_PORT}) >/dev/null 2>&1
+    if [[ $? -eq 0 ]]; then
+        printf " done!\n"
+        break
+    fi
+    printf '.'
+    sleep 1
+done
 python manage.py collectstatic --noinput
+python manage.py migrate
 exec "$@"
