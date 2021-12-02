5a8346ca89e637f673bfd113834800bf4e61fb6e
Qwizi <ciolek.adrian@protonmail.com>
2021-12-02 22:56:37 +0100

    changes in Docker

--- server/entrypoint.prod.sh
+++ server/entrypoint.prod.sh
@@ -1,20 +1,4 @@
 #!/bin/sh
-
-DB_NAME=cenzopapa
-DB_PORT=5432
-
-printf "Waiting for psql to boot..."
-
-for _ in `seq 0 100`; do
-    (echo > /dev/tcp/${DB_NAME}/${DB_PORT}) >/dev/null 2>&1
-    if [[ $? -eq 0 ]]; then
-        printf " done!\n"
-        break
-    fi
-    printf '.'
-    sleep 1
-done
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-python manage.py migrate
-python manage.py loaddata initial_images_data.json
 exec "$@"
